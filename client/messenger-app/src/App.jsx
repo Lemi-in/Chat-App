f4b8bc4ec35b2ddf3c2026e556b44b8c94a6c04d
*Lemi-in* <*lemesaelias@gmail.com*>
2024-08-22 15:34:51 +0300

    project completed

---
+++ client/messenger-app/src/App.jsx
@@ -0,0 +1,55 @@
+import './App.css';
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+import Chat from './Chat';
+
+const socket = io('http://localhost:3001', {
+  transports: ['websocket', 'polling']
+});
+
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+  
+
+  const joinRoom = () => {
+      if (username !== "" && room !== "") {
+          socket.emit('join_room', room);
+        setShowChat(true);
+      }
+  }
+  return (
+    
+    <>
+    <div className='App'> 
+     { !showChat ? (
+      <div className="joinChatContainer">
+
+        <h3> Join A chat</h3>
+        <input type="text" 
+        placeholder='John.....' 
+        onChange={(e) => {
+          setUsername(e.target.value)
+        }}/> 
+        <input type="text" 
+        placeholder='Room ID...'
+        onChange={(e) => {
+          setRoom(e.target.value)
+        }}/>
+        <button onClick={joinRoom}>Join A Room</button>
+
+        </div>
+  )
+        : (
+        <Chat socket={socket} username={username} room={room}/>
+        ) }
+      </div>
+      
+      </>
+    
+  )
+}
+
+export default App
